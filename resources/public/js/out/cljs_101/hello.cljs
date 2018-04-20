078d3baf1c5e46d275b52338f22aaba69216223c
Pedro Bautista <pbautista@localhost.localdomain>
2018-04-20 10:31:14 -0500

    medio bien

--- resources/public/js/out/cljs_101/hello.cljs
+++ resources/public/js/out/cljs_101/hello.cljs
@@ -15,11 +15,126 @@
 ;(def jsondata (t/read (t/reader :json) (.readFileSync fs "/home/pbautista/primerjson.json" "utf8")))
 ;(def r (t/reader :primerjson.json))
 
-(go (let [response (<! (http/get "http://localhost:3000/cams"))]
-      (.log js/console (str (:body response)))))
+
+
+;
+; (def camaras-json (go (let [response (<! (http/get "http://localhost:3000/cams"))]
+;       ;(.log js/console (str (:body response)))
+;     ;response
+; ;(.log js/console (str  response))
+; ;response
+;
+; ;(def cuerpo (str (:body response)))
+; ;(.log js/console  cuerpo)
+;
+; (let [m1 (:body response)]
+; (.log js/console (str m1))
+;
+; )
+;
+;
+;       )
+;
+;       ))
+
+(defn right-triangle2? [[a b c]]
+  (let [a-square (* a a)
+        b-square (* b b)
+        c-square (* c c)]
+    (= (+ a-square b-square) c-square))
+    )
+
+
+; (defn prueba []
+;
+;
+; (go (let [coca (<! (http/get "http://localhost:3000/cams"))]
+; ;(.log js/console (str (:body coca)))
+; ;(:body coca)
+;
+; (let [ju (str (:body coca))]
+; (.log js/console (str ju))
+;
+; )
+;
+; ))
+;
+;
+;
+;   )
+
+(go (def pepsi(  <! (http/get "http://localhost:3000/cams")))
+
+(def select(:body pepsi))
+;(.log js/console (str select))
+;(js/getJSON select)
+
+)
+
+(.log js/console (str select))
+
+(defn choise-webcam []
+  [:div.mdl-textfield.mdl-js-textfield.getmdl-select
+   [:input#sample1.mdl-textfield__input
+    ]
+   [:input {:name "sample1", :value "", :type "hidden"}]
+   [:label.mdl-textfield__label {:for "sample1"} "Choise webcam"]
+   [:ul.mdl-menu.mdl-menu--bottom-left.mdl-js-menu
+    {:for "sample1"}
+    [:li.mdl-menu__item {:data-val "camara 1" :on-click (fn [x] (.log js/console (-> x .-currentTarget (.getAttribute "data-val"))))} "Webcam1"]
+    [:li.mdl-menu__item {:data-val "BLR"} "cam 2"]
+    [:li.mdl-menu__item {:data-val "RUS"} "Webcam3"]]
+    ])
+
+;  (def cola (prueba))
+
+;(.log js/console  (JSON.stringify cola)       )
+;(.log js/console (str cola))
+
+; (defn dd [] (go (let [response (<! (http/get "http://localhost:3000/cams"))]
+;       ;(.log js/console (:status response))
+;       ;(.log js/console (str (:body response)))
+;
+;       (let [m1 (:body response)]
+; (.log js/console "esto" + (str m1))
+;
+;       )
+; (:body response)
+;
+;
+;       )
+;
+;       ;(:body response)
+;        ))
+;
+;
+; (def jeje (dd))
+;
+; (.log js/console (str jeje))
 
 
 
+; (defn return-cams (go (let [response (<! (http/get "http://localhost:3000/cams"))]
+; ;(let [m1 (:body response)]
+; ;(.log js/console (str m1))
+; ;m1
+; ;)
+;       )
+;       ))
+
+
+
+
+
+;(.log js/console (type  camaras-json) )
+
+;
+; (def cama (go    (let [resp (<! (http/get "http://localhost:3000/cams")]) )))
+;
+; (.log js/console (str camaras))
+
+
+;(.log js/console  cuerpo)
             ; (defn getnames []
             ;
             ; (let [names (json/read-str (slurp "/home/pbautista/primerjson.json"))]
@@ -81,23 +196,91 @@
   []
   [mdl/button
   ;[:i {:class "material-icons"} "camera alt"]
+  :type "button"
    :raised?  true
    :colored? true
-   :child    "Capturar"]
+   :ripple-effect? true
+   :child    "Capturar"
+   :disabled? false
+   ;:on-click (http://localhost:3000/foto)
+
+
+   :on-click fboton
+    ;:on-click #(do (.preventDefault %) (nav! "http://localhost:3000/foto"))
+    ; )
+    ;:href "http://wwww.google.com"
+   ]
    )
 
+;(def web )
 
-(defn choise-webcam []
-  [:div.mdl-textfield.mdl-js-textfield.getmdl-select
-   [:input#sample1.mdl-textfield__input
-    ]
-   [:input {:name "sample1", :value "", :type "hidden"}]
-   [:label.mdl-textfield__label {:for "sample1"} "Choise webcam"]
-   [:ul.mdl-menu.mdl-menu--bottom-left.mdl-js-menu
-    {:for "sample1"}
-    [:li.mdl-menu__item {:data-val "DEU" :on-click (fn [x] (.log js/console (-> x .-currentTarget (.getAttribute "data-val"))))} "Webcam1"]
-    [:li.mdl-menu__item {:data-val "BLR"} "Webcam2"]
-    [:li.mdl-menu__item {:data-val "RUS"} "Webcam3"]]])
+(defn fboton []
+
+(.log js/console "pulso boton")
+(redirect! "http://localhost:3000/foto")
+;(.open window "http://localhost:3000/foto")
+
+  )
+  (defn fboton2 []
+
+  (.log js/console "pulso boton")
+  (redirect! "http://localhost:3000/foto2")
+  ;(.open window "http://localhost:3000/foto")
+
+    )
+
+
+
+  (defn redirect! [loc] (set! (.-location js/window) loc))
+
+
+
+  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+  (defn button2
+ "This is a raised button that is colored."
+ []
+ [mdl/button
+ ;[:i {:class "material-icons"} "camera alt"]
+ :type "button"
+ :raised?  true
+ :colored? true
+ :ripple-effect? true
+ :child    "Capturar 2"
+ :disabled? false
+ ;:on-click (http://localhost:3000/foto)
+
+
+ :on-click fboton2
+ ;:on-click #(do (.preventDefault %) (nav! "http://localhost:3000/foto"))
+ ; )
+ ;:href "http://wwww.google.com"
+ ]
+ )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+; (defn boton-ejemplo []
+;   [:form
+;      {:method "get",
+;       :action "https://www.w3schools.com/js/js_json_html.asp"}
+;      [:button {:type "submit"} "Submit"]
+;
+;   )
+
 
 
     (defn jumbo[]
@@ -155,6 +338,10 @@
         (reagent/render [text-field-demo-text] (.getElementById js/document "app"))
         (reagent/render [choise-webcam] (.getElementById js/document "choiseWebcam"))
         (reagent/render [button-demo-colored-raised] (.getElementById js/document "button"))
-        (reagent/render [jumbo] (.getElementById js/document "jumbotron1"))
+        (reagent/render [button2] (.getElementById js/document "button2"))
+        (reagent/render [jumbo] (.getElementById js/document "jumbotron1")
+
+
+        )
         ;(reagent/render [getnames] (.getElementById js/document "jumbotron1"))
       )
--- resources/public/js/out/cljs_101/hello.js
+++ resources/public/js/out/cljs_101/hello.js
@@ -5,100 +5,120 @@ goog.require('reagent.core');
 goog.require('re_mdl.core');
 goog.require('cljs_http.client');
 goog.require('cljs.core.async');
-var c__23283__auto___24287 = cljs.core.async.chan.call(null,(1));
-cljs.core.async.impl.dispatch.run.call(null,((function (c__23283__auto___24287){
+cljs_101.hello.right_triangle2_QMARK_ = (function cljs_101$hello$right_triangle2_QMARK_(p__26972){
+var vec__26973 = p__26972;
+var a = cljs.core.nth.call(null,vec__26973,(0),null);
+var b = cljs.core.nth.call(null,vec__26973,(1),null);
+var c = cljs.core.nth.call(null,vec__26973,(2),null);
+var a_square = (a * a);
+var b_square = (b * b);
+var c_square = (c * c);
+return cljs.core._EQ_.call(null,(a_square + b_square),c_square);
+});
+var c__23245__auto___26990 = cljs.core.async.chan.call(null,(1));
+cljs.core.async.impl.dispatch.run.call(null,((function (c__23245__auto___26990){
 return (function (){
-var f__23284__auto__ = (function (){var switch__23260__auto__ = ((function (c__23283__auto___24287){
-return (function (state_24281){
-var state_val_24282 = (state_24281[(1)]);
-if((state_val_24282 === (1))){
-var inst_24274 = cljs_http.client.get.call(null,"http://localhost:3000/cams");
-var state_24281__$1 = state_24281;
-return cljs.core.async.impl.ioc_helpers.take_BANG_.call(null,state_24281__$1,(2),inst_24274);
+var f__23246__auto__ = (function (){var switch__23222__auto__ = ((function (c__23245__auto___26990){
+return (function (state_26983){
+var state_val_26984 = (state_26983[(1)]);
+if((state_val_26984 === (1))){
+var inst_26976 = cljs_http.client.get.call(null,"http://localhost:3000/cams");
+var state_26983__$1 = state_26983;
+return cljs.core.async.impl.ioc_helpers.take_BANG_.call(null,state_26983__$1,(2),inst_26976);
 } else {
-if((state_val_24282 === (2))){
-var inst_24276 = (state_24281[(2)]);
-var inst_24277 = new cljs.core.Keyword(null,"body","body",-2049205669).cljs$core$IFn$_invoke$arity$1(inst_24276);
-var inst_24278 = [cljs.core.str.cljs$core$IFn$_invoke$arity$1(inst_24277)].join('');
-var inst_24279 = console.log(inst_24278);
-var state_24281__$1 = state_24281;
-return cljs.core.async.impl.ioc_helpers.return_chan.call(null,state_24281__$1,inst_24279);
+if((state_val_26984 === (2))){
+var inst_26978 = (state_26983[(2)]);
+var inst_26979 = cljs_101.hello.pepsi = inst_26978;
+var inst_26980 = new cljs.core.Keyword(null,"body","body",-2049205669).cljs$core$IFn$_invoke$arity$1(cljs_101.hello.pepsi);
+var inst_26981 = cljs_101.hello.select = inst_26980;
+var state_26983__$1 = (function (){var statearr_26985 = state_26983;
+(statearr_26985[(7)] = inst_26979);
+
+return statearr_26985;
+})();
+return cljs.core.async.impl.ioc_helpers.return_chan.call(null,state_26983__$1,inst_26981);
 } else {
 return null;
 }
 }
-});})(c__23283__auto___24287))
+});})(c__23245__auto___26990))
 ;
-return ((function (switch__23260__auto__,c__23283__auto___24287){
+return ((function (switch__23222__auto__,c__23245__auto___26990){
 return (function() {
-var cljs_101$hello$state_machine__23261__auto__ = null;
-var cljs_101$hello$state_machine__23261__auto____0 = (function (){
-var statearr_24283 = [null,null,null,null,null,null,null];
-(statearr_24283[(0)] = cljs_101$hello$state_machine__23261__auto__);
+var cljs_101$hello$state_machine__23223__auto__ = null;
+var cljs_101$hello$state_machine__23223__auto____0 = (function (){
+var statearr_26986 = [null,null,null,null,null,null,null,null];
+(statearr_26986[(0)] = cljs_101$hello$state_machine__23223__auto__);
 
-(statearr_24283[(1)] = (1));
+(statearr_26986[(1)] = (1));
 
-return statearr_24283;
+return statearr_26986;
 });
-var cljs_101$hello$state_machine__23261__auto____1 = (function (state_24281){
+var cljs_101$hello$state_machine__23223__auto____1 = (function (state_26983){
 while(true){
-var ret_value__23262__auto__ = (function (){try{while(true){
-var result__23263__auto__ = switch__23260__auto__.call(null,state_24281);
-if(cljs.core.keyword_identical_QMARK_.call(null,result__23263__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
+var ret_value__23224__auto__ = (function (){try{while(true){
+var result__23225__auto__ = switch__23222__auto__.call(null,state_26983);
+if(cljs.core.keyword_identical_QMARK_.call(null,result__23225__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
 continue;
 } else {
-return result__23263__auto__;
+return result__23225__auto__;
 }
 break;
 }
-}catch (e24284){if((e24284 instanceof Object)){
-var ex__23264__auto__ = e24284;
-var statearr_24285_24288 = state_24281;
-(statearr_24285_24288[(5)] = ex__23264__auto__);
+}catch (e26987){if((e26987 instanceof Object)){
+var ex__23226__auto__ = e26987;
+var statearr_26988_26991 = state_26983;
+(statearr_26988_26991[(5)] = ex__23226__auto__);
 
 
-cljs.core.async.impl.ioc_helpers.process_exception.call(null,state_24281);
+cljs.core.async.impl.ioc_helpers.process_exception.call(null,state_26983);
 
 return new cljs.core.Keyword(null,"recur","recur",-437573268);
 } else {
-throw e24284;
+throw e26987;
 
 }
 }})();
-if(cljs.core.keyword_identical_QMARK_.call(null,ret_value__23262__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
-var G__24289 = state_24281;
-state_24281 = G__24289;
+if(cljs.core.keyword_identical_QMARK_.call(null,ret_value__23224__auto__,new cljs.core.Keyword(null,"recur","recur",-437573268))){
+var G__26992 = state_26983;
+state_26983 = G__26992;
 continue;
 } else {
-return ret_value__23262__auto__;
+return ret_value__23224__auto__;
 }
 break;
 }
 });
-cljs_101$hello$state_machine__23261__auto__ = function(state_24281){
+cljs_101$hello$state_machine__23223__auto__ = function(state_26983){
 switch(arguments.length){
 case 0:
-return cljs_101$hello$state_machine__23261__auto____0.call(this);
+return cljs_101$hello$state_machine__23223__auto____0.call(this);
 case 1:
-return cljs_101$hello$state_machine__23261__auto____1.call(this,state_24281);
+return cljs_101$hello$state_machine__23223__auto____1.call(this,state_26983);
 }
 throw(new Error('Invalid arity: ' + (arguments.length - 1)));
 };
-cljs_101$hello$state_machine__23261__auto__.cljs$core$IFn$_invoke$arity$0 = cljs_101$hello$state_machine__23261__auto____0;
-cljs_101$hello$state_machine__23261__auto__.cljs$core$IFn$_invoke$arity$1 = cljs_101$hello$state_machine__23261__auto____1;
-return cljs_101$hello$state_machine__23261__auto__;
+cljs_101$hello$state_machine__23223__auto__.cljs$core$IFn$_invoke$arity$0 = cljs_101$hello$state_machine__23223__auto____0;
+cljs_101$hello$state_machine__23223__auto__.cljs$core$IFn$_invoke$arity$1 = cljs_101$hello$state_machine__23223__auto____1;
+return cljs_101$hello$state_machine__23223__auto__;
 })()
-;})(switch__23260__auto__,c__23283__auto___24287))
+;})(switch__23222__auto__,c__23245__auto___26990))
 })();
-var state__23285__auto__ = (function (){var statearr_24286 = f__23284__auto__.call(null);
-(statearr_24286[(6)] = c__23283__auto___24287);
+var state__23247__auto__ = (function (){var statearr_26989 = f__23246__auto__.call(null);
+(statearr_26989[(6)] = c__23245__auto___26990);
 
-return statearr_24286;
+return statearr_26989;
 })();
-return cljs.core.async.impl.ioc_helpers.run_state_machine_wrapped.call(null,state__23285__auto__);
-});})(c__23283__auto___24287))
+return cljs.core.async.impl.ioc_helpers.run_state_machine_wrapped.call(null,state__23247__auto__);
+});})(c__23245__auto___26990))
 );
 
+console.log([cljs.core.str.cljs$core$IFn$_invoke$arity$1(cljs_101.hello.select)].join(''));
+cljs_101.hello.choise_webcam = (function cljs_101$hello$choise_webcam(){
+return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.mdl-textfield.mdl-js-textfield.getmdl-select","div.mdl-textfield.mdl-js-textfield.getmdl-select",95083235),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input#sample1.mdl-textfield__input","input#sample1.mdl-textfield__input",-254253907)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"name","name",1843675177),"sample1",new cljs.core.Keyword(null,"value","value",305978217),"",new cljs.core.Keyword(null,"type","type",1174270348),"hidden"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"label.mdl-textfield__label","label.mdl-textfield__label",-1621295497),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"for","for",-1323786319),"sample1"], null),"Choise webcam"], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"ul.mdl-menu.mdl-menu--bottom-left.mdl-js-menu","ul.mdl-menu.mdl-menu--bottom-left.mdl-js-menu",-1176294735),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"for","for",-1323786319),"sample1"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"li.mdl-menu__item","li.mdl-menu__item",-598444289),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"data-val","data-val",-1410637751),"camara 1",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (x){
+return console.log(x.currentTarget.getAttribute("data-val"));
+})], null),"Webcam1"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"li.mdl-menu__item","li.mdl-menu__item",-598444289),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"data-val","data-val",-1410637751),"BLR"], null),"cam 2"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"li.mdl-menu__item","li.mdl-menu__item",-598444289),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"data-val","data-val",-1410637751),"RUS"], null),"Webcam3"], null)], null)], null);
+});
 cljs_101.hello.text_field_demo_text = (function cljs_101$hello$text_field_demo_text(){
 return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [re_mdl.core.textfield,new cljs.core.Keyword(null,"floating-label?","floating-label?",-1615247461),true,new cljs.core.Keyword(null,"label","label",1718410804),"Ruta"], null);
 });
@@ -106,12 +126,26 @@ return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMP
  * This is a raised button that is colored.
  */
 cljs_101.hello.button_demo_colored_raised = (function cljs_101$hello$button_demo_colored_raised(){
-return new cljs.core.PersistentVector(null, 7, 5, cljs.core.PersistentVector.EMPTY_NODE, [re_mdl.core.button,new cljs.core.Keyword(null,"raised?","raised?",145778912),true,new cljs.core.Keyword(null,"colored?","colored?",1847962275),true,new cljs.core.Keyword(null,"child","child",623967545),"Capturar"], null);
+return new cljs.core.PersistentVector(null, 15, 5, cljs.core.PersistentVector.EMPTY_NODE, [re_mdl.core.button,new cljs.core.Keyword(null,"type","type",1174270348),"button",new cljs.core.Keyword(null,"raised?","raised?",145778912),true,new cljs.core.Keyword(null,"colored?","colored?",1847962275),true,new cljs.core.Keyword(null,"ripple-effect?","ripple-effect?",1342045356),true,new cljs.core.Keyword(null,"child","child",623967545),"Capturar",new cljs.core.Keyword(null,"disabled?","disabled?",-1523234181),false,new cljs.core.Keyword(null,"on-click","on-click",1632826543),cljs_101.hello.fboton], null);
 });
-cljs_101.hello.choise_webcam = (function cljs_101$hello$choise_webcam(){
-return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div.mdl-textfield.mdl-js-textfield.getmdl-select","div.mdl-textfield.mdl-js-textfield.getmdl-select",95083235),new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input#sample1.mdl-textfield__input","input#sample1.mdl-textfield__input",-254253907)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"name","name",1843675177),"sample1",new cljs.core.Keyword(null,"value","value",305978217),"",new cljs.core.Keyword(null,"type","type",1174270348),"hidden"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"label.mdl-textfield__label","label.mdl-textfield__label",-1621295497),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"for","for",-1323786319),"sample1"], null),"Choise webcam"], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"ul.mdl-menu.mdl-menu--bottom-left.mdl-js-menu","ul.mdl-menu.mdl-menu--bottom-left.mdl-js-menu",-1176294735),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"for","for",-1323786319),"sample1"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"li.mdl-menu__item","li.mdl-menu__item",-598444289),new cljs.core.PersistentArrayMap(null, 2, [new cljs.core.Keyword(null,"data-val","data-val",-1410637751),"DEU",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (x){
-return console.log(x.currentTarget.getAttribute("data-val"));
-})], null),"Webcam1"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"li.mdl-menu__item","li.mdl-menu__item",-598444289),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"data-val","data-val",-1410637751),"BLR"], null),"Webcam2"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"li.mdl-menu__item","li.mdl-menu__item",-598444289),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"data-val","data-val",-1410637751),"RUS"], null),"Webcam3"], null)], null)], null);
+cljs_101.hello.fboton = (function cljs_101$hello$fboton(){
+console.log("pulso boton");
+
+return cljs_101.hello.redirect_BANG_.call(null,"http://localhost:3000/foto");
+});
+cljs_101.hello.fboton2 = (function cljs_101$hello$fboton2(){
+console.log("pulso boton");
+
+return cljs_101.hello.redirect_BANG_.call(null,"http://localhost:3000/foto2");
+});
+cljs_101.hello.redirect_BANG_ = (function cljs_101$hello$redirect_BANG_(loc){
+return window.location = loc;
+});
+/**
+ * This is a raised button that is colored.
+ */
+cljs_101.hello.button2 = (function cljs_101$hello$button2(){
+return new cljs.core.PersistentVector(null, 15, 5, cljs.core.PersistentVector.EMPTY_NODE, [re_mdl.core.button,new cljs.core.Keyword(null,"type","type",1174270348),"button",new cljs.core.Keyword(null,"raised?","raised?",145778912),true,new cljs.core.Keyword(null,"colored?","colored?",1847962275),true,new cljs.core.Keyword(null,"ripple-effect?","ripple-effect?",1342045356),true,new cljs.core.Keyword(null,"child","child",623967545),"Capturar 2",new cljs.core.Keyword(null,"disabled?","disabled?",-1523234181),false,new cljs.core.Keyword(null,"on-click","on-click",1632826543),cljs_101.hello.fboton2], null);
 });
 cljs_101.hello.jumbo = (function cljs_101$hello$jumbo(){
 return new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"jumbotron text-center"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),"Selfie WebCam"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"p","p",151049309),"hola bootstrap"], null)], null);
@@ -128,8 +162,10 @@ reagent.core.render.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.co
 
 reagent.core.render.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs_101.hello.button_demo_colored_raised], null),document.getElementById("button"));
 
+reagent.core.render.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs_101.hello.button2], null),document.getElementById("button2"));
+
 return reagent.core.render.call(null,new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs_101.hello.jumbo], null),document.getElementById("jumbotron1"));
 });
 goog.exportSymbol('cljs_101.hello.init', cljs_101.hello.init);
 
-//# sourceMappingURL=hello.js.map?rel=1524151585488
+//# sourceMappingURL=hello.js.map?rel=1524237821043
